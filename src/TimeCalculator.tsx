6d0c2113f39b48fdff311e358d9cf5bba8daff9f
Severalpens <Paul.Collins@ibm.com>
2022-02-24 14:38:14 +1100

    refactor

--- src/TimeCalculator.tsx
+++ src/TimeCalculator.tsx
@@ -1,99 +1,98 @@
 import timetables from "./timetables";
 
 export default class TimeCalculator {
-    date: any;
-    today: any;
-    midnight: any;
-    tomorrow: any;
-    midnightTonight: any;
+    timetableToday: Array<any>;
+    timetableTomorrow: Array<any>;
+    midnightEpochSeconds: number;
+    midnightTonightEpochSeconds: number;
     locationId: number;
     directionId: number;
 
-    constructor(){
+    constructor() {
         this.locationId = -1;
-        this.directionId =-1;
+        this.directionId = -1;
+        this.midnightEpochSeconds = -1;
+        this.midnightTonightEpochSeconds = -1;
+        this.timetableToday = [];
+        this.timetableTomorrow = [];
     }
 
 
 
     getMidnight() {
-        let date  = new Date();
+        let date = new Date();
         date.setHours(0, 0, 0, 0);
         return date.getTime() / 1000;
     }
 
+    getFormattedTime(s: number) {
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s % 60;
+        var hrs = (s - mins) / 60;
+
+        return hrs ? hrs + ':' + mins + ':' + secs : hrs + ':' + mins + ':' + secs;
+    }
+
 
     setTimetable(dayId: number, locationId: number, directionId: number) {
-        return timetables.filter(x => x.dayId === dayId && x.locationId === locationId && x.directionId === directionId);
-    }
-
-
-    getEpochSeconds() {
-        let now = Math.floor(this.date.getTime() / 1000);
-        return now;
-    }
-
-
-    getNextTimetableSeconds() {
-        let timesRemainingToday = this.today.filter((t: { secondsFromMidnight: any; }) => {
-            let tEpochSeconds = this.midnight + t.secondsFromMidnight;
-            return tEpochSeconds > Math.floor(this.date.getTime() / 1000);;
+        return timetables.filter(x => {
+            let isDay = x.dayId === dayId;
+            let isLoc = x.locationId === locationId;
+            let isDir = x.directionId === directionId;
+            return isDay && isLoc && isDir;
         });
-
-        return  timesRemainingToday[0] ? this.midnight + timesRemainingToday[0].secondsFromMidnight : this.midnight + (24 * 60 * 60) + this.tomorrow[0].secondsFromMidnight;
-
     }
 
     
-getFormattedTime(s: number) {
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
-  
-    return hrs ? hrs + ':' + mins + ':' + secs : hrs + ':' + mins + ':' + secs ;
-  }
-  
+    getNextTimetableSeconds(dt: Date) {
+        let timesRemainingToday = this.timetableToday.filter((t: { secondsFromMidnight: number; }) => {
+            let tEpochSeconds = this.midnightEpochSeconds + t.secondsFromMidnight;
+            return tEpochSeconds > Math.floor(dt.getTime() / 1000);
+        });
 
-
-    secondsTilNext() {
-        let nextTimetableSecondsFromMidnight = this.getNextTimetableSeconds();
-        let nextEpochSeconds = this.midnightTonight + nextTimetableSecondsFromMidnight;
-        let nowEpochSeconds = Math.floor(this.date.getTime() / 1000);
-        return nextEpochSeconds - nowEpochSeconds;
+        if(timesRemainingToday !== []){
+            return this.midnightEpochSeconds + timesRemainingToday[0].secondsFromMidnight;
+        }
+        else{
+            //If no times remaining today, get first arrival from tomorrow's timetable.
+            let nextMidnight = this.midnightEpochSeconds + (24 * 60 * 60);
+            let firstArrivalTomorrow = this.timetableTomorrow[0].secondsFromMidnight;
+            return nextMidnight + firstArrivalTomorrow;
+        }
     }
 
+
+
+
     next(locationId: number, directionId: number) {
-        this.date = new Date();
-        this.midnight = this.getMidnight();
-        this.locationId = locationId;
-        this.directionId = directionId;
+        let dt = new Date();
+        this.midnightEpochSeconds = this.getMidnight();
 
-        
-        switch (new Date().getDay()) {
-            case 5:
-                this.today = this.setTimetable(1, this.locationId, this.directionId);
-                this.tomorrow = this.setTimetable(6, this.locationId, this.directionId);
+        switch (dt.getDay()) {
+            case 5: //Friday
+                this.timetableToday = this.setTimetable(1, locationId, directionId);
+                this.timetableTomorrow = this.setTimetable(6, locationId, directionId);
                 break;
 
-            case 6:
-                this.today = this.setTimetable(6, this.locationId, this.directionId);
-                this.tomorrow = this.setTimetable(0, this.locationId, this.directionId);
+            case 6: //Saturday
+                this.timetableToday = this.setTimetable(6, locationId, directionId);
+                this.timetableTomorrow = this.setTimetable(0, locationId, directionId);
                 break;
 
-            case 0:
-                this.today = this.setTimetable(0, this.locationId, this.directionId);
-                this.tomorrow = this.setTimetable(1, this.locationId, this.directionId);
+            case 0: //Sunday
+                this.timetableToday = this.setTimetable(0, locationId, directionId);
+                this.timetableTomorrow = this.setTimetable(1, locationId, directionId);
                 break;
 
-            default:
-                this.today = this.setTimetable(1, this.locationId, this.directionId);
-                this.tomorrow = this.setTimetable(1, this.locationId, this.directionId);
+            default: // Other weekdays
+                this.timetableToday = this.setTimetable(1, locationId, directionId);
+                this.timetableTomorrow = this.setTimetable(1, locationId, directionId);
                 break;
         }
 
-        let nowEpochSeconds = Math.floor(this.date.getTime() / 1000);
-        let s = this.getNextTimetableSeconds() - nowEpochSeconds;
+        let nowEpochSeconds = Math.floor(dt.getTime() / 1000);
+        let s = this.getNextTimetableSeconds(dt) - nowEpochSeconds;
         return this.getFormattedTime(s);
     }
 
